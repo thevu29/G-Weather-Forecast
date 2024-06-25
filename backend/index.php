b8c18ad86698024a5ee775cefc914d9b6b8936d2
thevu29 <vnguyen132az@gmail.com>
2024-06-25 14:29:28 +0700

    weather forecast

--- backend/index.php
+++ backend/index.php
@@ -2,11 +2,19 @@
 require './vendor/autoload.php';
 require './src/Weather.php';
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+try {
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    
+    if (isset($_GET['location'])) {
+        $location = $_GET['location'] !== '' ? $_GET['location'] : 'Ho Chi Minh';
+    } else {
+        $location = 'Ho Chi Minh';
+    }
 
-$location = isset($_GET['location']) ? $_GET['location'] : 'Ho Chi Minh';
-$weatherAPI = new Weather('2d3af1d1640a4237b9374444242406');
-
-$weatherData = $weatherAPI->getWeather($location);
-echo json_encode($weatherData);
+    $weatherAPI = new Weather('2d3af1d1640a4237b9374444242406');
+    $weatherData = $weatherAPI->getWeather($location);
+    echo json_encode($weatherData);
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+}
--- backend/src/Weather.php
+++ backend/src/Weather.php
@@ -5,11 +5,34 @@ class Weather {
 
     public function __construct($apiKey) {
         $this->apiKey = $apiKey;
-    } 
+    }
 
     public function getWeather($location) {
-        $url = "https://api.weatherapi.com/v1/current.json?key=" . urlencode($this->apiKey) . "&q=" . urlencode($location) . "&aqi=no";
-        $response = file_get_contents($url);
+        $url = 'https://api.weatherapi.com/v1/forecast.json?key=' . urlencode($this->apiKey) . '&q=' . urlencode($location) . '&days=4&aqi=no&alerts=no';
+        // $url = 'https://api.weatherapi.com/v1/forecast.json?key=2d3af1d1640a4237b9374444242406&q=Ho Chi Minh&days=4&aqi=no&alerts=no';
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if ($httpCode != 200) {
+            $error = json_decode($response, true);
+            if (isset($error['error'])) {
+                return $error;
+            } else {
+                return [
+                    'error' => [
+                        'code' => $httpCode,
+                        'message' => 'An unknown error occurred.'
+                    ]
+                ];
+            }
+        }
+
+        curl_close($curl);
         return json_decode($response, true);
     }
 }
