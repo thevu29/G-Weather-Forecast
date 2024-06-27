07f489f291ce95eaf1c77c8fdfd94774e29f66a3
thevu29 <vnguyen132az@gmail.com>
2024-06-27 14:27:19 +0700

    fix config.php

--- backend/src/config.php
+++ backend/src/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('WEATHER_API_KEY', getenv('WEATHER_API_KEY'));
+define('WEATHER_API_KEY', getenv('WEATHER_API_KEY') ?: '2d3af1d1640a4237b9374444242406');
 define('DB_HOST', getenv('DB_HOST'));
 define('DB_PORT', getenv('DB_PORT'));
 define('DB_NAME', getenv('DB_NAME'));
