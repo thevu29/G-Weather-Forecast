3cf97bb8f9f7752c7715fd6e601794660a1d69af
thevu29 <vnguyen132az@gmail.com>
2024-06-27 12:47:57 +0700

    fix deploy config

--- backend/src/config.php
+++ backend/src/config.php
@@ -1,8 +1,8 @@
 <?php
 
 define('WEATHER_API_KEY', getenv('WEATHER_API_KEY'));
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_PORT', getenv('DB_PORT'));
 define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_PORT', getenv('DB_PORT'));
