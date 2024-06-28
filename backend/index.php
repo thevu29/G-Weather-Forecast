dcec302d094f341e0ac3d68a7439fd1347dfee05
thevu29 <vnguyen132az@gmail.com>
2024-06-28 15:53:26 +0700

    check log

--- backend/index.php
+++ backend/index.php
@@ -1,25 +1,23 @@
 <?php
+$host = 'weather_forecast.dpg-cpv30btumphs73c4o7e0-a.singapore-postgres.render.com';
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_DATABASE_NAME');
+$user = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
 
-$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+echo $dsn . '<br>';
 
-switch ($requestUri) {
-    case '/weather':
-        require './src/getWeather.php';
-        break;
-    case '/send-email':
-        require './src/sendEmail.php';
-        break;
-    case '/subscribe':
-        require './src/subscribe.php';
-        break;
-    case '/unsubscribe':
-        require './src/unsubscribe.php';
-        break;
-    default:
-        http_response_code(404);
-        echo '404 Not Found';
-        break;
+try {
+    $pdo = new PDO($dsn);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to the database successfully!" . "<br>"; 
+
+    $sql = "SELECT * FROM Subscriber";
+    $stmt = $pdo->query($sql);
+    $subscribers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r($subscribers);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
 }
