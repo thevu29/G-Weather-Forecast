85ff8894d4006918c3accc02ef1b3f1f4eea2314
thevu29 <vnguyen132az@gmail.com>
2024-06-28 16:00:54 +0700

    check log 2

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,29 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($requestUri) {
+    case '/weather':
+        require './src/getWeather.php';
+        break;
+    case '/send-email':
+        require './src/sendEmail.php';
+        break;
+    case '/subscribe':
+        require './src/subscribe.php';
+        break;
+    case '/unsubscribe':
+        require './src/unsubscribe.php';
+        break;
+    default:
+        http_response_code(404);
+        echo '404 Not Found';
+        break;
+}
+
 $host = 'weather_forecast.dpg-cpv30btumphs73c4o7e0-a.singapore-postgres.render.com';
 $port = getenv('DB_PORT');
 $dbname = getenv('DB_DATABASE_NAME');
